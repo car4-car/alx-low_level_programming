aec87f0fce04c427800c47137b78475835e1b1e2
caroline wanjiru <cw35677807@gmail.com>
2022-09-10 06:00:22 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-
- * main . entry point
- *
- * Return: Always 0 (successful)
- */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0)
+	return (0);
 }
