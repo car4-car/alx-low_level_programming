2c4ace808cb8026834f05415ab30d566beffca79
caroline wanjiru <cw35677807@gmail.com>
2022-09-14 12:37:10 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,18 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n)
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-voidnum(int r, int n);
 
-#endif
+#endif`
