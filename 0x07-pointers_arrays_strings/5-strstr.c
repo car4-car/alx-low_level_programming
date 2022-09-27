4c6eff88426185fc323fcb5fa7f5a48a327fc590
Caroline Wanjiru <cw35677807@gmail.com>
2022-09-27 03:30:05 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  _strstr - returns pointer to first char of matching substring
- *  @haystack: string to find substring in
- *  @needle: substring to find match of
- *  Return: pointer to first char of matching substring
+ * _strstr - returns pointer to first char of matching substring
+ * @haystack: string to find substring in
+ * @needle: substring to find match of
+ * Return: pointer to first char of matching substring
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -16,9 +16,9 @@ char *_strstr(char *haystack, char *needle)
 		k = 0;
 		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
 			haystack++, needle++, k++;
-		if (*needle == '\0'
-				return (haystack - k);
-				haystack -= (k - 1), needle -= k;
+		if (*needle == '\0')
+			return (haystack - k);
+		haystack -= (k - 1), needle -= k;
 	}
 	return ('\0');
 }
